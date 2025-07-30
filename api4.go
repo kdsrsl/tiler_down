b87435812dd21f7b6e997b73ee5bd3df739bd75e
senRsl <senrsl@aol.com>
2025-07-30 15:25:18 +0800

    up

--- api4.go
+++ api4.go
@@ -3,28 +3,54 @@ package main
 import (
 	"os"
 	"fmt"
+	"log"
 	"path"
+	"time"
 	"runtime"
 	"io/ioutil"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
+	gin.SetMode(gin.ReleaseMode)
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.Printf("endpoint is %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+	gin.DisableConsoleColor()
+	r := gin.New()
+	//r.SetTrustedProxies(nil)
+	r.SetTrustedProxies([]string{"127.0.0.1"})
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+	//fmt.Println(gin.Mode())
+	// r.GET("/ping", func(c *gin.Context) {
+ //        c.JSON(200, gin.H{
+ //            "message": "pong",
+ //        })
+ //    })
     r.GET("/tile/:z/:x/:y",tile)
     r.Run(":9971")
 }
 
 func tile(c *gin.Context){
-    exPath := getCurrentAbPathByCaller()
-    fmt.Println(exPath)
+    //exPath := getCurrentAbPathByCaller()
+    exPath := ""
+    //fmt.Println(exPath)
 	//c.JSON(200,gin.H{"msg":c.Param("z")+","+c.Param("x")+","+c.Param("y")})
-	mPath := "/out"
+	// mPath := "/out"
+	mPath := "out"
 	file := fmt.Sprintf("%s/%s/%s/%s.jpg",exPath+mPath,c.Param("z"),c.Param("x"),c.Param("y"))
 	fmt.Println(file,isExists(file))
 	if !isExists(file){
